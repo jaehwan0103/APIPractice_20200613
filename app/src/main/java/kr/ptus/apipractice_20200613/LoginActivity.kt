8dcc64fd604201f276e671fc9f2cd98ef0e970e4
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-13 12:35:40 +0900

    로그인 결과 분석 + 실패 화면 처리  - 로그인 결과 : code 의 값이 200인가?  - 실패시 : 실패사유 message를 받아서 토스트로 출력

--- app/src/main/java/kr/ptus/apipractice_20200613/LoginActivity.kt
+++ app/src/main/java/kr/ptus/apipractice_20200613/LoginActivity.kt
@@ -1,8 +1,11 @@
 package kr.ptus.apipractice_20200613
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 import kr.ptus.apipractice_20200613.util.ServerUtil
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -20,7 +23,29 @@ class LoginActivity : BaseActivity() {
             val inputEmail = emailEdt.text.toString()
             val inputPw = pwEdt.text.toString()
 
-            ServerUtil.postRequestLogin(mContext,inputEmail,inputPw,null)
+            ServerUtil.postRequestLogin(
+                mContext,
+                inputEmail,
+                inputPw,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+                        Log.d("화면에서 보는 응답", json.toString())
+
+                        val codeNum = json.getInt("code")
+
+                        if (codeNum == 200) {
+
+                        } else {
+                            val message = json.getString("message")
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+
+                    }
+
+                })
 
         }
 
