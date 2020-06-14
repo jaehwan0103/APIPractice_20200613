0c3e5248c02b217ecf78f1b9275f75383a57e5c0
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-14 16:18:38 +0900

    댓글 파싱 코드 추가중  => 작성자를 ...

--- app/src/main/java/kr/ptus/apipractice_20200613/data/TopicReply.kt
+++ app/src/main/java/kr/ptus/apipractice_20200613/data/TopicReply.kt
@@ -8,7 +8,17 @@ class TopicReply {
 
         fun getTopicReplyFromJson(json : JSONObject)  : TopicReply {
 
+            val  tr = TopicReply()
 
+            tr.id = json.getInt("id")
+            tr.content = json.getString("content")
+            tr.topicId = json.getInt("topic_id")
+            tr.sideId = json.getInt("side_id")
+            tr.userId = json.getInt("user_id")
+
+
+
+            return tr
 
 
         }
