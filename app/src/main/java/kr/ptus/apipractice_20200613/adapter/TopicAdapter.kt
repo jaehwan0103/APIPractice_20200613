a4d273bbdca6dbd52bb0bcd9249c0e30598b1bbe
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-14 12:48:07 +0900

    파싱된 이미지를 뿌려줬다

--- app/src/main/java/kr/ptus/apipractice_20200613/adapter/TopicAdapter.kt
+++ app/src/main/java/kr/ptus/apipractice_20200613/adapter/TopicAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import kr.ptus.apipractice_20200613.R
 import kr.ptus.apipractice_20200613.data.Topic
 
@@ -23,6 +26,15 @@ class TopicAdapter(val mContext : Context , val resId : Int , val mList : List<T
         }
 
         val row = tempRow!!
+
+        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
+        val topicTitleTxt = row.findViewById<TextView>(R.id.topicTitleTxt)
+
+        val data = mList[position]
+
+        topicTitleTxt.text = data.title
+
+        Glide.with(mContext).load(data.imageUrl).into(topicImg)
 
         return row
 
