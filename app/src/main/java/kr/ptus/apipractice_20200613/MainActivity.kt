3de63d37e29dbaf02d507eb370e4fb5588a0cb03
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-14 11:49:44 +0900

    로그아웃 처리  - 저장 되었던 토큰을 빈칸으로 되돌리기

--- app/src/main/java/kr/ptus/apipractice_20200613/MainActivity.kt
+++ app/src/main/java/kr/ptus/apipractice_20200613/MainActivity.kt
@@ -1,8 +1,12 @@
 package kr.ptus.apipractice_20200613
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.ptus.apipractice_20200613.util.ContextUtil
 import kr.ptus.apipractice_20200613.util.ServerUtil
 import org.json.JSONObject
 
@@ -17,9 +21,27 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        logoutBtn.setOnClickListener {
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("로그아웃 확인")
+            alert.setMessage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                ContextUtil.setuserToken(mContext,"")
+
+                val myIntent = Intent(mContext , LoginActivity::class.java)
+                startActivity(myIntent)
+
+                finish()
+
+            })
+            alert.setNegativeButton("취소",null)
+            alert.show()
+        }
+
     }
 
     override fun setValues() {
+
 
         ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(json: JSONObject) {
@@ -29,14 +51,14 @@ class MainActivity : BaseActivity() {
                 val nickName = user.getString("nick_name")
 
                 runOnUiThread {
-                loginUserNickNameTxt.text = "${nickName}님 환영합니다."
+                    loginUserNickNameTxt.text = "${nickName}님 환영합니다."
+                }
             }
-        }
 
 
-    })
+        })
 
 
-}
+    }
 
 }
