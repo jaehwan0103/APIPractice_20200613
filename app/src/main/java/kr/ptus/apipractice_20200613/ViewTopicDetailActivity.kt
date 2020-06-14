c0a1467df1693246f3a2a974a49c4b58d2125ec3
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-14 14:21:12 +0900

    그림도 출력했다

--- app/src/main/java/kr/ptus/apipractice_20200613/ViewTopicDetailActivity.kt
+++ app/src/main/java/kr/ptus/apipractice_20200613/ViewTopicDetailActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
 import kr.ptus.apipractice_20200613.data.Topic
 import kr.ptus.apipractice_20200613.util.ServerUtil
@@ -55,6 +56,8 @@ class ViewTopicDetailActivity : BaseActivity() {
 //                    받아온 주제의 제목을 화면에 표시
                     runOnUiThread{
                     topicTitleTxt.text = mTopic.title
+
+                        Glide.with(mContext).load(mTopic.imageUrl).into(topicImg)
                     }
                 }
 
