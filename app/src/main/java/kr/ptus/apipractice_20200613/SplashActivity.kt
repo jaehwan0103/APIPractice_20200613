2f27e5c1d0c466b278b436d20e6bd3e5563d15d1
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-14 11:42:47 +0900

    3초후에 토큰 검사 => 메인화면으로 이동

--- app/src/main/java/kr/ptus/apipractice_20200613/SplashActivity.kt
+++ app/src/main/java/kr/ptus/apipractice_20200613/SplashActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import kr.ptus.apipractice_20200613.util.ContextUtil
+import kr.ptus.apipractice_20200613.util.ServerUtil
+import org.json.JSONObject
 
-class SplashActivity :BaseActivity() {
+class SplashActivity : BaseActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +29,36 @@ class SplashActivity :BaseActivity() {
         val myHandler = Handler()
         myHandler.postDelayed({
 
-            val myIntent = Intent(mContext, LoginActivity::class.java)
-            startActivity(myIntent)
+            if (ContextUtil.isAutoLogin(mContext)) {
+                if (ContextUtil.getUserToken(mContext) != "") {
+                    ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler {
+                        override fun onResponse(json: JSONObject) {
+                            val code = json.getInt("code")
 
-            finish()
+                            if (code == 200) {
+                                val myIntent = Intent(mContext, MainActivity::class.java)
+                                startActivity(myIntent)
+                            } else {
+                                val myIntent = Intent(mContext, LoginActivity::class.java)
+                                startActivity(myIntent)
+                            }
+                        }
+
+                    })
+                } else {
+
+                    val myIntent = Intent(mContext, LoginActivity::class.java)
+                    startActivity(myIntent)
+                }
+
+            } else {
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+
+                finish()
+            }
 
         }, 3000)
-
 
 
     }
